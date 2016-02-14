96501a268214c0097ecedb5f72eaa100e43529f2
cdelmoral <carderomad@gmail.com>
2016-02-14 14:42:37 -0600

    Update repository

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "//${REGISTRY_URL}/:_authToken=${AUTH_TOKEN}" > "${HOME}/.npmrc"
+#echo "//${REGISTRY_URL}/:_authToken=${AUTH_TOKEN}" > "${HOME}/.npmrc"
+#npm adduser --registry ${REGISTRY_URL}
 npm publish;
 npm version prerelease --no-git-tag-version;
